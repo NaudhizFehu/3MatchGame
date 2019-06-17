77fd3a0c53f86154bc6a8e0f19df34eafe6d536b
이태훈 <xogns520@naver.com>
2019-06-18 02:41:35 +0900

    Add Panel Animation
    Add Panel Animation

--- Assets/02_Scripts/SoundManager.cs
+++ Assets/02_Scripts/SoundManager.cs
@@ -6,11 +6,18 @@ public class SoundManager : MonoBehaviour
 {
     public AudioSource[] destroyNoise;
 
+    public AudioSource BGM;
+
     public void PlayRandomDestoryNoise()
     {
         //Choos a random number
         int clipToPlay = 0;//Random.Range(0, destroyNoise.Length);
         //play that clip
         destroyNoise[clipToPlay].Play();
+    }
+
+    public void playBGM()
+    {
+        BGM.Play();
     }
 }
